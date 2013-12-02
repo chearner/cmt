db2538f9f18f47d97a50d8fb6eb0d2be4bb7b221
Fayded <kevin.d.fay@gmail.com>
2013-12-02 16:56:18 -0500

    update to forward slash

--- app/js/controllers/AppController.js
+++ app/js/controllers/AppController.js
@@ -5,24 +5,24 @@ cmtApp.controller('AppController',
         $scope.ui = {
             logo: {
                 title: 'Party Down South',
-                url: '/img/logo-pds.png'
+                url: 'img/logo-pds.png'
             },
             pics: {
                 title: 'Redneck',
-                url: '/img/jdw-0001.jpg'
+                url: 'img/jdw-0001.jpg'
             },
             icons: {
                 jesus: {
                     title: 'Jesus',
-                    url: '/img/icon-jesus.png',
+                    url: 'img/icon-jesus.png',
                 },
                 tape: {
                     title: 'Duct Tape',
-                    url: '/img/icon-tape.png'
+                    url: 'img/icon-tape.png'
                 },
                 whiskey: {
                     title: 'Whiskey',
-                    url: '/img/icon-whiskey.png'
+                    url: 'img/icon-whiskey.png'
                 }
             }
         }
