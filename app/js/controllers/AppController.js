98b93d75c2d25656036aac139b9c1f32c012e29b
Chris Hearn <chearner@gmail.com>
2013-12-02 16:30:08 -0500

    1st attempt.

---
+++ app/js/controllers/AppController.js
@@ -0,0 +1,30 @@
+'use strict';
+
+cmtApp.controller('AppController',
+    function AppController($scope) {
+        $scope.ui = {
+            logo: {
+                title: 'Party Down South',
+                url: '/img/logo-pds.png'
+            },
+            pics: {
+                title: 'Redneck',
+                url: '/img/jdw-0001.jpg'
+            },
+            icons: {
+                jesus: {
+                    title: 'Jesus',
+                    url: '/img/icon-jesus.png',
+                },
+                tape: {
+                    title: 'Duct Tape',
+                    url: '/img/icon-tape.png'
+                },
+                whiskey: {
+                    title: 'Whiskey',
+                    url: '/img/icon-whiskey.png'
+                }
+            }
+        }
+    }
+);
