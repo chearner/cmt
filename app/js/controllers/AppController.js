e3869a5b400073d8b9f3dcf5d1d219fd82666989
Chris Hearn <chearner@gmail.com>
2013-12-04 16:27:22 -0500

    Updates to main image..

--- app/js/controllers/AppController.js
+++ app/js/controllers/AppController.js
@@ -1,16 +1,54 @@
 'use strict';
 
+cmtApp.service('picService', function ($http) {
+    this.getPicsPaged = function (nodeId, pageNumber, take) {
+        //return $http.get('myUrl');
+    };
+});
+
+
 cmtApp.controller('AppController',
     function AppController($scope) {
+        init();
+
+        function init() {
+            alert('start');
+            //picService.getPicsPaged(1234, 1, 1).then(function (data) {
+                //$scope.feedbackItems = data;
+            //});
+        }
+
+        $scope.picCount = 0;
+
+        $scope.$watch("picCount", function (count) {
+            if (count > 5) {
+                alert('count max');
+                $scope.picCount = 5;
+            } else if (count < 0) {
+                alert('count min');
+                $scope.picCount = 0;
+            }
+        });
+
+        $scope.prevPic = function () {
+            $scope.picCount--;
+        };
+
+        $scope.nextPic = function () {
+            $scope.picCount++;
+        };
+
         $scope.ui = {
             logo: {
                 title: 'Party Down South',
                 url: 'img/logo-pds.png'
             },
-            pics: {
-                title: 'Redneck',
-                url: 'img/jdw-0001.jpg'
-            },
+            pics: [
+                {
+                    title: 'Redneck 1',
+                    url: 'img/jdw-0001.jpg'
+                }
+            ],
             icons: {
                 jesus: {
                     title: 'Jesus',
